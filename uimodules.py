c49441ef020c1d866df61626844f78b04e508c21
Michael V. DePalatis <mike@depalatis.net>
2016-06-29 22:12:44 +0200

    Formatting

--- uimodules.py
+++ uimodules.py
@@ -2,11 +2,12 @@ from os import path
 from tornado.web import UIModule
 from tornado.escape import xhtml_unescape
 
+_basepath = path.join(path.abspath(path.dirname(__file__)), "templates")
+
 
 class VueTemplate(UIModule):
     def render(self, filename):
-        fname = path.join(path.abspath(path.dirname(__file__)), "templates", filename)
-        print(fname)
+        fname = path.join(_basepath, filename)
         with open(fname) as f:
             contents = xhtml_unescape(f.read())
         return self.render_string("vue-template.html",
