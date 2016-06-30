f2d5358199fdfa6a8d3e7b81f307d04614969a9b
Michael V. DePalatis <mike@depalatis.net>
2016-06-30 20:13:19 +0200

    Remove unnecessary unescaping

--- uimodules.py
+++ uimodules.py
@@ -1,6 +1,5 @@
 from os import path
 from tornado.web import UIModule
-from tornado.escape import xhtml_unescape
 
 _basepath = path.join(path.abspath(path.dirname(__file__)), "templates")
 
@@ -9,6 +8,6 @@ class VueTemplate(UIModule):
     def render(self, filename):
         fname = path.join(_basepath, filename)
         with open(fname) as f:
-            contents = xhtml_unescape(f.read())
+            contents = f.read()
         return self.render_string("vue-template.html",
                                   id="test-template", contents=contents)
